1b339655c5c09ff8854172fec23488ed5e7e2a92
eldrinch <eldrinc@hotmail.com>
2021-09-19 02:39:47 -0300

    trabalho pratico

---
+++ trabpratico/src/app/selecao/selecao.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "selecao",
+  templateUrl: "./selecao.component.html",
+  styleUrls: ["./selecao.component.css"],
+})
+export class SelecaoComponent  {
+  @Input()
+  titulo: string = "";
+  @Input()
+  opcoes: string[] = [];
+  @Input()
+  escolhaAte: number = 1;
+//estado interno do componente
+opcoesSelecionadas: string[] = [];
+
+
+  constructor() {}
+
+  
+  seleciona(opcao:string){
+   
+    if (this.escolhaAte === 1){
+      this.opcoesSelecionadas= [opcao]
+    }else{
+
+      const idx = this.opcoesSelecionadas.indexOf(opcao)
+
+      if(idx === -1){
+        this.opcoesSelecionadas.push(opcao)
+      }else {
+      this.opcoesSelecionadas.splice(idx,1)
+      }
+    }
+  }
+
+  estaDesabilitado(opcao:string):boolean{
+    const idx = this.opcoesSelecionadas.indexOf(opcao)
+    return this.escolhaAte > 1 &&
+      idx === -1 && 
+      this.opcoesSelecionadas.length >= this.escolhaAte;
+  }
+}
